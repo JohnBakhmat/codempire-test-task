ee61001a09291427b2a2d9e2631cd19926968b4f
Yevhenii <yevhenii.bakhmat@nure.ua>
2021-12-24 12:27:38 +0200

    Started working on search component

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,22 @@
+import React, { ChangeEventHandler } from 'react'
+import style from './style.module.sass'
+interface Props {
+  placeholder?: string
+  onChange?: ChangeEventHandler<HTMLInputElement>
+}
+
+const Search = (props: Props) => {
+  return (
+    <div className={style['search']}>
+      <input
+        type='search'
+        className={style['search-input']}
+        placeholder={props.placeholder}
+        onChange={props.onChange}
+      />
+      <i className='fas fa-search' />
+    </div>
+  )
+}
+
+export default Search
