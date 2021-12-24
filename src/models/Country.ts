941e1350a0ec6b6949a45cfa13f47b00411957ad
Yevhenii <yevhenii.bakhmat@nure.ua>
2021-12-24 19:46:40 +0200

    Added id field for country

--- src/models/Country.ts
+++ src/models/Country.ts
@@ -1,10 +1,12 @@
 export default class Country {
+	public id: string
 	public Name: string
 	public TotalConfirmed: number
 	public TotalDeaths: number
 	public TotalRecoverd: number
 
-	constructor(name: string, totalConfirmed: number, totalDeaths: number, totalRecovered: number) {
+	constructor(id: string, name: string, totalConfirmed: number, totalDeaths: number, totalRecovered: number) {
+		this.id = id
 		this.Name = name
 		this.TotalConfirmed = totalConfirmed
 		this.TotalDeaths = totalDeaths
